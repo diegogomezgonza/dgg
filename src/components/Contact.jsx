84d9060dfc9808969328fbb58add23291b6932ca
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2023-11-24 19:20:11 +0100

    Contenido para contact y color cambiado en hover

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 const Contact = () => {
+  const handleMouseOver = (site) => {
+    console.log(`Mouse over ${site}`);
+  };
+
   return (
-    <section>
-      <h2 className="text-white mt-5">Contacto</h2>
-    </section>
+    <div className="contact-section gap-5">
+      <div className="contact-site text-white" onMouseOver={() => handleMouseOver("Site 1")}>
+        <h3>LinkedIn</h3>
+        <p className="additional-text">Mi experiencia completa.</p>
+      </div>
+
+      <div className="contact-site text-white" onMouseOver={() => handleMouseOver("Site 2")}>
+        <h3>Github</h3>
+        <p className="additional-text">Un vistazo a mis proyectos</p>
+      </div>
+    </div>
   );
 };
 
