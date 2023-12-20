07a86c1e2f28d6446d065eef006063319e340a2b
Diego Gómez González <93382813+diegogomezgonza@users.noreply.github.com>
2023-12-20 19:35:35 +0100

    Aspecto estático al carrusel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
+import "./css/Navbar.css"; // Import your CSS file
 
 const Navbar = () => {
   return (
     <nav className="navbar text-center p-5 position-fixed end-0">
       <ul className="navbar-list">
         <li className="navbar-item">
-          <Link to="/draws">DC</Link>
+          <Link to="/draws" className="dcLink">DC</Link>
         </li>
       </ul>
     </nav>
